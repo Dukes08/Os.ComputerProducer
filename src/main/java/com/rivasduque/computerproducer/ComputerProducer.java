aa5ba8f423fe1ff51052f502823c20a16af10611
Hugo Duque <hugo.duque@correo.unimet.edu.ve>
2024-09-29 23:12:17 -0400

    first commit

---
+++ src/main/java/com/rivasduque/computerproducer/ComputerProducer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.rivasduque.computerproducer;
+
+/**
+ *
+ * @author hugoduque
+ */
+public class ComputerProducer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
